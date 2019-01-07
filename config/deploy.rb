d19d602644d22360eed274708ff8488706ac35cd
duongpham910 <duongpham91094@gmail.com>
2019-01-07 18:09:19 +0700

    config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.7.2"
+lock "3.11.0"
 
 set :application, "rails_girls"
 set :repo_url, "git@github.com:duongpham910/rails_girls.git"
@@ -10,6 +10,9 @@ set :bundle_binstubs, nil
 set :branch, "develop"
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/#{fetch(:application)}"
+
+# set :tmp_dir, '/tmp'
+set :local_user, 'framgia'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -22,10 +25,11 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 # set :pty, trues
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
-
+set :linked_files, fetch(:linked_files, [])
+  .push("config/database.yml", "config/secrets.yml")
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+set :linked_dirs, fetch(:linked_dirs, [])
+  .push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle")
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
